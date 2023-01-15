85509521aaf5cd4001210f8ba0c527e234acea8c
VishalPawar1010 <vshalofficial@gmail.com>
2023-01-15 18:37:09 +0530

    wip: added role and its repo layer
    1. added jpa and sqlConnector dependecies
    2. Added user role and repo

--- ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/MainController.java
+++ ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/MainController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
 	
-	@GetMapping("/")
+	@GetMapping("/ShopmeAdmin")
 	public String viewHomePage() {
 		return "index";
 	}
